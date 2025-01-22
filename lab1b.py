4622263f9367b4d26991a77981f02b2020a54787
Vanshika Dawar <vdawar@myseneca.ca>
2025-01-22 18:46:09 -0500

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''Descrption: This program stores a string in an object and prints it.'''
+#Define a string variable
+name = 'Isaac'
+#Print the output with concatenation
+print('How old are you ' + name + '?')
