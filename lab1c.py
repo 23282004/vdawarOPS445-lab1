8fb7743e27e214235a41e73b7b60c15b69c43130
Vanshika Dawar <vdawar@myseneca.ca>
2025-01-22 18:57:06 -0500

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: this script stores an integer and a string and prints them'''
+# Define a string variable
+name = 'Isaac'
+# Define an integer variable
+age = 72
+# Print the formatted output
+print(name + ' is ' + str(age) + ' years old!' )
