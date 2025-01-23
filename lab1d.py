57f7112cbdd40f2b7c4fb86cb7b4021af7933536
Vanshika Dawar <vdawar@myseneca.ca>
2025-01-22 19:10:57 -0500

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description : this program demostrates mathematical operations in python'''
+# Define different variables which include x,y and z
+x = 10
+y = 2
+z = 5
+# performing the calculation
+result = x + y * z
+print(f"{x} + {y} * {z} = {result}")
